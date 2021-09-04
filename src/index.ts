323644cdb91ded2601b6995db6e3704dd97876df
Yuku Kotani <yukukotani@gmail.com>
2021-09-04 23:32:02 +0900

    show completion

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,36 @@
-console.log('Hello World');
+import TsServerLibrary from 'typescript/lib/tsserverlibrary';
+
+function init({ typescript: ts }: { typescript: typeof TsServerLibrary }) {
+  function create(info: ts.server.PluginCreateInfo) {
+    const log = (...params: unknown[]) => {
+      const text = params.map((p) => (p ? JSON.stringify(p) : p)).join(' ');
+      info.project.projectService.logger.info(`[namespace-import] ${text}`);
+    };
+
+    log('Start init');
+
+    const getCompletionsAtPosition = info.languageService.getCompletionsAtPosition;
+    info.languageService.getCompletionsAtPosition = (fileName, position, options) => {
+      log('getCompletionsAtPosition', { fileName, position, options });
+      const original = getCompletionsAtPosition(fileName, position, options);
+      if (original == null) {
+        return original;
+      }
+
+      const entry: ts.CompletionEntry = {
+        name: 'TestService',
+        kind: ts.ScriptElementKind.alias,
+        source: '/Users/yukukotani/ghq/github.com/ubie-inc/yukustory/packages/client/src/TestService.ts',
+        sortText: 'TestService',
+        hasAction: true,
+      };
+      original.entries.push(entry);
+
+      return original;
+    };
+  }
+
+  return { create };
+}
+
+export = init;
