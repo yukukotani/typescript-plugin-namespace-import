bae639cbb94d63e82582c347447035f67cd763f5
Yuku Kotani <yukukotani@gmail.com>
2021-09-05 21:34:45 +0900

    Add example

---
+++ example/src/index.ts
@@ -0,0 +1,5 @@
+import * as SomeLogics from './logics/SomeLogics';
+
+SomeLogics.divide(4, 2);
+
+// You can auto-complete `AnotherLogics` here!
---
+++ example/src/logics/AnotherLogics.ts
@@ -0,0 +1,7 @@
+export function print(msg: any) {
+  console.log(msg);
+}
+
+export function printError(error: any) {
+  console.error(error);
+}
